5d28346e91ac766019b161d27b01e803fd3a9238
Adam Fidel <adam@fidel.id>
2025-02-05 17:38:17 -0600

    remove excess prints

--- pyinfra/files/launch-rtorrent.sh
+++ pyinfra/files/launch-rtorrent.sh
@@ -4,5 +4,5 @@ if ! tmux list-sessions >/dev/null 2>&1; then
     echo "No tmux sessions found. Starting rtorrent"
     tmux new-session -d -s rtorrent "rtorrent"
 else
-    echo "A tmux session is already running. Doing nothing."
+    # a tmux session is already running -- doing nothing
 fi
